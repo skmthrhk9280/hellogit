da0caca85f857c1929facefecaeb6d8f17bac315
いわし <siosazanami@gmail.com>
2020-04-24 13:53:50 +0900

    add a new function to input and output user name

--- hellogit.c
+++ hellogit.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
 int main(void){
+    char username[16];
     printf("hello, git!\n");
+    
+    printf("What's your name?\n");
+    scanf("%s",username);
+    printf("Your name is %s!\n",username);
 return 0;
 }
